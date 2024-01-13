3417c1a68b99eafebfed5e814af00c3be1596bc1
alperengokbak <swalperen3008@gmail.com>
2024-01-13 04:25:08 +0300

    Added new allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://prescription-api-gateway.onrender.com, https://prescription-frontend.onrender.com/"];
 
 export { allowedOrigins };
