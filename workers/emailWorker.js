a6299570ec0b840dc4a1efa05282e036991c1441
alperengokbak <swalperen3008@gmail.com>
2024-01-13 23:03:03 +0300

    Changed amqp connection string

--- workers/emailWorker.js
+++ workers/emailWorker.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const sendToRabbitMQ = async (message) => {
   try {
     const connection = await amqp.connect(
-      `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}:5672/`
+      `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`
     );
     console.log("Connected to RabbitMQ");
 
